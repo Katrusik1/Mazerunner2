0356729f8712f928e75b245be8160622b0603c14
digmag <dmitryslabukhin@gmail.com>
2023-06-26 22:54:38 +0700

    asd

--- Assets/Scripts/Spawnenemy.cs
+++ Assets/Scripts/Spawnenemy.cs
@@ -6,16 +6,23 @@ public class Spawnenemy : MonoBehaviour
 {
     public GameObject[] enemy;
     public GameObject[] spawningPoints;
+    private bool canspawn = true;
     private void OnTriggerEnter(Collider other)
     {
-        spawn();
+        if (canspawn)
+        {
+            spawn();
+            canspawn = false;
+        }
     }
 
     private void spawn()
     {
         for(int i = 0; i < spawningPoints.Length; i++)
         {
-            //
+            Debug.Log(enemy.Length);
+            GameObject newenemy = Instantiate(enemy[Random.Range(0, enemy.Length)]);
+            newenemy.transform.position = spawningPoints[i].transform.position;
         }
     }
 }
